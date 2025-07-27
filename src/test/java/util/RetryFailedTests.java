73761606a098b188cc3926a61e19286cada28593
lhansamali <lakmini.hansamali@gmail.com>
2025-07-27 21:37:35 +0530

    Added Retry listener

---
+++ src/test/java/util/RetryFailedTests.java
@@ -0,0 +1,25 @@
+package util;
+
+import lombok.extern.log4j.Log4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+@Log4j
+public class RetryFailedTests implements IRetryAnalyzer {
+
+    private static final Logger log = LoggerFactory.getLogger(RetryFailedTests.class);
+    private int reRetryCount = 0;
+    private static final int MAX_RETRY_COUNT = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (reRetryCount < MAX_RETRY_COUNT) {
+            log.info("Retrying Failed tests: {}", iTestResult.getTestName());
+            reRetryCount++;
+            return true;
+        }
+        return false;
+    }
+}
